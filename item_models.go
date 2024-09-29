31598bbb25a7982ed527b075fdede68f0437680c
Tech <70489278+TechDahTurtle@users.noreply.github.com>
2024-09-28 20:58:30 -0400

    Added Armor Models

--- item_models.go
+++ item_models.go
@@ -15,6 +15,10 @@ func writeItemModelJSONs(namespace string, resource_name string) {
 	writeToolModelJson(namespace, resource_name, "sword")
 
 	// Write the armor JSON files.
+	writeArmorModelJson(namespace, resource_name, "boots")
+	writeArmorModelJson(namespace, resource_name, "chestplate")
+	writeArmorModelJson(namespace, resource_name, "helmet")
+	writeArmorModelJson(namespace, resource_name, "leggings")
 }
 
 /**
@@ -33,4 +37,22 @@ func writeToolModelJson(namespace string, resource_name string, tool_type string
 		"  }\n" +
 		"}"
 	writeJsonFile(resource_name+"_"+tool_type+".json", content)
+}
+
+/**
+ * Write the armor JSON files for the given resource name and namespace.
+ *
+ * @param namespace The namespace for the mod.
+ * @param resource_name The name of the resource.
+ * @param armor_type The type of armor.
+ */
+func writeArmorModelJson(namespace string, resource_name string, armor_type string) {
+	// Write the armor item model JSON file.
+	content := "{\n" +
+		"  \"parent\": \"item/generated\",\n" +
+		"  \"textures\": {\n" +
+		"    \"layer0\": \"" + namespace + ":items/" + resource_name + "_" + armor_type + "\"\n" +
+		"  }\n" +
+		"}"
+	writeJsonFile(resource_name+"_"+armor_type+".json", content)
 }
