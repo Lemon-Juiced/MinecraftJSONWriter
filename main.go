16f87bd2b7a60203b60b15b75a8119b21bec565e
Lemon-Juiced <47193272+Lemon-Juiced@users.noreply.github.com>
2024-09-28 20:24:59 -0400

    Added doc comment

--- main.go
+++ main.go
@@ -5,6 +5,18 @@ import (
 	"os"
 )
 
+/**
+ * JSONWriter is an application that takes a command line argument containing a namespace, the name of a resource, and a -i, -t, or -c flag denoting an item or tag (either in the given or common directory).
+ * It then reads the arguments and writes the data to several JSON files.
+ * These JSON files are for Minecraft models and recipes, for a set of tools and armor.
+ * The JSON files are written to the correct directories, based on the given namespace for a NeoForge mod.
+ *
+ * The command line arguments are as follows:
+ * make
+ * mcjsonwriter <namespace> <resource_name> <-i, -t, or -c>
+ *
+ * @author Lemon_Juiced & TechDahTurtle
+ */
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Please provide arguments.")
